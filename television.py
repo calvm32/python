c9a86f7c7f9f6d2448a50990203f87f0edfe8a1a
calvm32 <cal.muon32@gmail.com>
2024-04-20 18:27:03 -0500

    Main Branch edits

--- television.py
+++ television.py
@@ -1,8 +1,4 @@
-class Television:
-    MIN_VOLUME = 0
-    MAX_VOLUME = 2
-    MIN_CHANNEL = 0
-    MAX_CHANNEL = 3
+class Television(MIN_VOLUME = 0, MAX_VOLUME = 2, MIN_CHANNEL = 0, MAX_CHANNEL = 3):
     
     def __init__(self):
         self.__status = False
